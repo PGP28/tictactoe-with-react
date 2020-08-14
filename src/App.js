f805706fec5b808491815e2759e87365e233d87d
Paul Guzmán <andres22.pgpa@gmail.com>
2020-08-14 18:03:52 -0400

    up-2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <>
+      <div>
+        <h1>Tic Tac Toe in React.js</h1>
+        <h2>Pick a Weapon</h2>
+      </div>
+      <div>
+
+        <div class="card">
+          <h2>CHOOSE YOUR WEAPON</h2>
+          <div class="input-group" >
+            <div>
+              <input type="text" aria-label="First name" class="form-control" placeholder="Player 1 username" /> <br />
+              <button type="button" class="btn btn-dark"><i class="fas fa-times"></i></button>
+            </div>
+            <div>
+              <input type="text" aria-label="Last name" class="form-control" placeholder="Player 2 username" /> <br />
+              <button type="button" class="btn btn-dark"><i class="far fa-circle"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+    </>
   );
 }
 
